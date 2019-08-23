0055e4976c4aec0b72b8d0c52d80b33dbe7053c6
Jaime Andres Moreno Toro <jmoreno084@gmail.com>
2019-08-23 00:08:15 -0500

    Creación del header y de los archivos (CRU) para navegación en el sitio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Productos from './components/Productos';
+import EditarProducto from './components/EditarProducto';
+import AgregarProducto from './components/AgregarProducto';
+import Producto from './components/Producto';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Header />
+        <main className="container mt-5">
+          <Switch>
+            <Route exact path="/nuevo-producto" component={AgregarProducto}/>
+            <Route exact path="/productos" component={Productos}/>
+            <Route exact path="/productos/:id" component={Producto}/>
+            <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+          </Switch>
+          <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
+        </main>
+      </Router>
   );
 }
 
---
+++ src/components/Producto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Producto() {
+    return (
+
+        <h1 className="text-center">Datos del Producto</h1>
+    );
+}
+
+export default Producto;
